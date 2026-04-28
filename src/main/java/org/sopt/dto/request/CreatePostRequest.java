89496a807228779010f184668bb021c8f83dff8d
1siis1 <sisees01@gmail.com>
2026-04-28 14:46:37 +0900

    chore: gitignore application.yml

--- src/main/java/org/sopt/dto/request/CreatePostRequest.java
+++ src/main/java/org/sopt/dto/request/CreatePostRequest.java
@@ -1,8 +1,5 @@
 package org.sopt.dto.request;
 
-import org.sopt.domain.BoardType;
-import org.sopt.domain.User;
-
 // 게시글 작성 요청 (클라이언트 → 서버)
 public record CreatePostRequest(
         String title,
