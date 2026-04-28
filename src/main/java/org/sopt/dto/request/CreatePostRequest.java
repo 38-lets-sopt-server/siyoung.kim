4404365755cf8420c3c6705c387cd2a2bb5609f7
1siis1 <sisees01@gmail.com>
2026-04-28 17:47:15 +0900

    fix: author (작성자 이름) CreatePostRequest 에서 삭제

--- src/main/java/org/sopt/dto/request/CreatePostRequest.java
+++ src/main/java/org/sopt/dto/request/CreatePostRequest.java
@@ -4,7 +4,6 @@ package org.sopt.dto.request;
 public record CreatePostRequest(
         String title,
         String content,
-        String author,
         String boardType,
         Long userId
 ) {}
