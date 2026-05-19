5931fc542e9ec68e606d7cd2eae029f6e1193cfa
1siis1 <sisees01@gmail.com>
2026-05-20 00:08:38 +0900

    feat: example 추가

--- src/main/java/org/sopt/auth/LoginRequest.java
+++ src/main/java/org/sopt/auth/LoginRequest.java
@@ -1,13 +1,16 @@
 package org.sopt.auth;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record LoginRequest (
         @NotBlank @Email
+        @Schema(description = "이메일", example = "hellot@hello.com")
         String email,
         @NotBlank
+        @Schema(description = "비밀번호", example = "1234")
         String password
 ){
 }
