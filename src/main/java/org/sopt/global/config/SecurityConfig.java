3c761057183c3f3134f4099c5d5b472fcec06100
1siis1 <sisees01@gmail.com>
2026-05-20 14:47:02 +0900

    feat: requestMatcher 경로 수정

--- src/main/java/org/sopt/global/config/SecurityConfig.java
+++ src/main/java/org/sopt/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.sopt.global.jwt.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,9 +26,13 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        // 로그인 및 토큰 재발급 api 는 인증 없이 접근 가능하도록
                         .requestMatchers("/api/v1/auth/login", "/api/v1/auth/reissue").permitAll()
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        // post 관련 get 메소드는 인증 없이 접근 가능하도록
+                        .requestMatchers(HttpMethod.GET, "/api/v1/posts", "/api/v1/posts/**").permitAll()
+                        // 유저 생성 api 도 인증 없이 가능하게(테스트용)
                         .requestMatchers("/api/v1/users").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
